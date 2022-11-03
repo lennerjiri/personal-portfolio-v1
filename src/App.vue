97147e1acac2d852e67f6a54453a39dcab63eb78
Jiří Lenner <ji.lenner@gmail.com>
2022-11-03 17:54:43 +0100

    functional loading animation, which waits until the page is loaded

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,71 @@
 <script setup>
-//import { RouterLink, RouterView } from "vue-router";
-// import HelloWorld from "./components/HelloWorld.vue";
+import { RouterLink, RouterView } from "vue-router";
+import { ref, onMounted, provide } from "vue";
+import LoadingScreen from "@/components/loadingScreen/loadingScreen.component.vue";
+
+// SVG Loading Screen
+const loading = ref(true);
+let openAnimation = false;
+const closeAnimation = ref(false);
+
+// Injection values to the SVG
+provide("closeAnimation", closeAnimation);
+provide("openAnimation", openAnimation);
+
+// Loading system
+const loadingFunction = () => {
+  // opening animation runs on its own once the loading screen is mounted
+  // set timeout waits until the animation plays and the page content is loaded
+  setTimeout(() => {
+    if (document.readyState === "complete" && openAnimation) {
+      closeAnimation.value = true;
+    } else {
+      // for that we use recursion
+
+      return loadingFunction();
+    }
+  }, 1000);
+};
+
+// Trigger loading system when app is mounted
+onMounted(() => {
+  document.onreadystatechange = () => {
+    loadingFunction();
+  };
+});
+
+// onLoading animation played emit
+const onLoadingCompleted = () => {
+  openAnimation = true;
+};
+
+// offLoading animation played emit
+const offLoadingCompleted = () => {
+  // close the loading screen
+  console.log("offLoadingCompleted");
+
+  loading.value = false;
+};
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <!-- <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink> -->
-      </nav>
-    </div>
-  </header>
-
-  <!-- <RouterView /> -->
+  <div class="root" :class="{ 'root-scrollOff': loading }">
+    <LoadingScreen
+      v-if="loading"
+      @onLoadingCompleted="onLoadingCompleted"
+      @offLoadingCompleted="offLoadingCompleted"
+    />
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+    <footer></footer>
+  </div>
 </template>
--- src/views/homeView/HomeView.component.vue
+++ src/views/homeView/HomeView.component.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="loading"></div>
 </template>
+
+<style></style>
