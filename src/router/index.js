ac63a836792c20f78542c50aaa9700530a576362
Jiří Lenner <ji.lenner@gmail.com>
2022-11-06 21:06:17 +0100

    finished technology page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/homeView/HomeView.component.vue";
+import TechnologiesView from "../views/technologiesView/TechnologiesView.component.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/technologies",
+      name: "technologies",
+      component: TechnologiesView,
     },
   ],
 });
